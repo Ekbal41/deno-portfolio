e656e2167cf300525ac2dfef1fb629f78af877a8
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-05-27 17:43:59 +0600

    u

--- islands/Techs.tsx
+++ islands/Techs.tsx
@@ -9,7 +9,7 @@ export default function Techs(props) {
           {props.desc}
         </p>
         <div class="row row-cols-1 row-cols-lg-3 mt-5">
-          <TechCard title={"Dano Runtime"} img={"https://ik.imagekit.io/serenity/ByteofDev/Blog_Heading_Images/State_of_the_Web_Deno"}/>
+          <TechCard title={"Deno Runtime"} img={"https://ik.imagekit.io/serenity/ByteofDev/Blog_Heading_Images/State_of_the_Web_Deno"}/>
           <TechCard title={"Fresh Framework"} img={"https://res.cloudinary.com/practicaldev/image/fetch/s--nNf3u14O--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6dre0h7reim1xq9eg87b.gif"}/>
         </div>
       </div>
